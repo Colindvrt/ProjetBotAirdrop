3ccf6757a36af1c81a83ab7ba8f9ffe9194f1322
Colin Dievart <colin.dievart@gmail.com>
2025-11-28 00:01:33 +0100

    init  plateformes

--- src/platforms/__init__.py
+++ src/platforms/__init__.py
@@ -4,4 +4,17 @@ Trading platform implementations.
 
 from .base import BasePlatformAPI, PlatformFactory
 
-__all__ = ['BasePlatformAPI', 'PlatformFactory']
+# Import all platform modules to register them with the factory
+from .hyperliquid.api import HyperliquidAPI
+from .paradex.api import ParadexAPI
+from .lighter.api import LighterAPI
+from .extended.api import ExtendedAPI
+
+__all__ = [
+    'BasePlatformAPI', 
+    'PlatformFactory',
+    'HyperliquidAPI',
+    'ParadexAPI', 
+    'LighterAPI',
+    'ExtendedAPI'
+]
